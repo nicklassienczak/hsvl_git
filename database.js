149ef97ac83264591f6346d2de5fbdf6ee79ee25
nicklassienczak <nicklas-sk@hotmail.com>
2022-11-16 09:01:44 +0100

    push

---
+++ database.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+// Read the environment variables
+require('dotenv').config();
+console.log(process.env.DATABASE)
+const mysqlConnection = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.MYSQL_USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    multipleStatements: true,
+});
+
+mysqlConnection.connect((err) => {
+    if (!err) {
+        console.log("Connected");
+    } else {
+        console.log(err);
+        console.log("Connection Failed");
+    }
+});
+
+module.exports = mysqlConnection;
