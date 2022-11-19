028488fbefabae517b9d4dc2683e4e0dfee64518
nicklassienczak <nicklas-sk@hotmail.com>
2022-11-19 17:20:36 +0100

    removed env.port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3030;
+const port = 3030;
 require('dotenv');
 const mysqlConnection = require("./database");
 
