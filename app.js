4cfdbf65aec7d4b38af48aa17bd404eb3ecf6663
arnefogh <andreasfogh7@gmail.com>
2022-11-21 19:31:25 +0100

    /data

--- app.js
+++ app.js
@@ -236,8 +236,8 @@ app.get('/favourites/:favourite_id', (req, res) => {
     );
 });
 
-app.get('/activitiesData', (req, res) => {
-    const query = "SELECT * FROM locations INNER JOIN cities ON cities.city_id = locations.city_id INNER JOIN activities ON activities.location_id = locations.location_id INNER JOIN seasons ON activities.season_id = seasons.season_id;";
+app.get('/data', (req, res) => {
+    const query = "SELECT * FROM locations INNER JOIN cities ON cities.city_id = locations.city_id INNER JOIN activities ON activities.location_id = locations.location_id INNER JOIN seasons ON activities.season_id = seasons.season_id INNER JOIN favourites ON favourites.activity_id = activities.activity_id INNER JOIN users ON users.user_id = favourites.user_id;";
     mysqlConnection.query(
         query,
         (err, results, fields) => {
